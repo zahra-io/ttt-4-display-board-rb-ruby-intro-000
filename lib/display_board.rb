539279b4404bffa2c07ccfc1d6d53471270f3dc1
Zahra Khan <zarssskhan@gmail.com>
2017-10-01 04:10:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 def display_board(board)
 
-  puts " #{board} | #{board} | #{board} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
