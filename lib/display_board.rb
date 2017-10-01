38821bc8749523b85fa8123279b55816300e739a
Zahra Khan <zarssskhan@gmail.com>
2017-10-01 04:13:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,5 +6,5 @@ def display_board(board)
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
